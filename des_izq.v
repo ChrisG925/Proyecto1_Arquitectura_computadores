478dc2e6d002480a9b51178644ce50c0b7526d0e
Tu Nombre <cgeorgi@miuandes.cl>
2026-09-06 01:36:03 -0300

    desplazamiento:_izq, arreglo error

--- des_izq.v
+++ des_izq.v
@@ -4,26 +4,27 @@ module desplazamiento_izq (
     output wire [3:0] S
 );
 
-    wire [3:0] shift1;
-    wire [3:0] shift2;
+    wire [3:0] s1;
+    wire [3:0] s2;
+    wire [3:0] s3;
 
-    // Desplazamiento de 1 posición
-    buf b10 (shift1[1], A[0]);
-    buf b11 (shift1[2], A[1]);
-    buf b12 (shift1[3], A[2]);
-    // shift1[0] = 0
+    // A << 1
+    buf b1_1 (s1[1], A[0]);
+    buf b1_2 (s1[2], A[1]);
+    buf b1_3 (s1[3], A[2]);
+    buf b1_0 (s1[0], 1'b0);
 
-    // Desplazamiento de 2 posiciones
-    buf b20 (shift2[2], A[0]);
-    buf b21 (shift2[3], A[1]);
-    // shift2[0] = 0
-    // shift2[1] = 0
+    // A << 2
+    buf b2_2 (s2[2], A[0]);
+    buf b2_3 (s2[3], A[1]);
+    buf b2_0 (s2[0], 1'b0);
+    buf b2_1 (s2[1], 1'b0);
 
-    // Selección según cantidad
-    // cantidad = 00 -> A
-    // cantidad = 01 -> A << 1
-    // cantidad = 10 -> A << 2
-    // cantidad = 11 -> A << 3
+    // A << 3
+    buf b3_3 (s3[3], A[0]);
+    buf b3_0 (s3[0], 1'b0);
+    buf b3_1 (s3[1], 1'b0);
+    buf b3_2 (s3[2], 1'b0);
 
     wire n0;
     wire n1;
@@ -37,46 +38,41 @@ module desplazamiento_izq (
     wire sel3;
 
     // 00
-    and a00_0 (sel0, n1, n0);
+    and a0 (sel0, n1, n0);
 
     // 01
-    and a01_0 (sel1, n1, cantidad[0]);
+    and a1 (sel1, n1, cantidad[0]);
 
     // 10
-    and a10_0 (sel2, cantidad[1], n0);
+    and a2 (sel2, cantidad[1], n0);
 
     // 11
-    and a11_0 (sel3, cantidad[1], cantidad[0]);
+    and a3 (sel3, cantidad[1], cantidad[0]);
 
-    // Salida
     wire [3:0] r0;
     wire [3:0] r1;
     wire [3:0] r2;
     wire [3:0] r3;
 
-    // cantidad = 00
-    and g00_0 (r0[0], A[0], sel0);
-    and g00_1 (r0[1], A[1], sel0);
-    and g00_2 (r0[2], A[2], sel0);
-    and g00_3 (r0[3], A[3], sel0);
+    and g00 (r0[0], A[0], sel0);
+    and g01 (r0[1], A[1], sel0);
+    and g02 (r0[2], A[2], sel0);
+    and g03 (r0[3], A[3], sel0);
 
-    // cantidad = 01
-    and g01_0 (r1[0], 1'b0, sel1);
-    and g01_1 (r1[1], shift1[1], sel1);
-    and g01_2 (r1[2], shift1[2], sel1);
-    and g01_3 (r1[3], shift1[3], sel1);
+    and g10 (r1[0], s1[0], sel1);
+    and g11 (r1[1], s1[1], sel1);
+    and g12 (r1[2], s1[2], sel1);
+    and g13 (r1[3], s1[3], sel1);
 
-    // cantidad = 10
-    and g10_0 (r2[0], 1'b0, sel2);
-    and g10_1 (r2[1], 1'b0, sel2);
-    and g10_2 (r2[2], shift2[2], sel2);
-    and g10_3 (r2[3], shift2[3], sel2);
+    and g20 (r2[0], s2[0], sel2);
+    and g21 (r2[1], s2[1], sel2);
+    and g22 (r2[2], s2[2], sel2);
+    and g23 (r2[3], s2[3], sel2);
 
-    // cantidad = 11 -> A << 3
-    and g11_0 (r3[0], 1'b0, sel3);
-    and g11_1 (r3[1], 1'b0, sel3);
-    and g11_2 (r3[2], 1'b0, sel3);
-    and g11_3 (r3[3], A[0], sel3);
+    and g30 (r3[0], s3[0], sel3);
+    and g31 (r3[1], s3[1], sel3);
+    and g32 (r3[2], s3[2], sel3);
+    and g33 (r3[3], s3[3], sel3);
 
     or o0 (S[0], r0[0], r1[0], r2[0], r3[0]);
     or o1 (S[1], r0[1], r1[1], r2[1], r3[1]);
